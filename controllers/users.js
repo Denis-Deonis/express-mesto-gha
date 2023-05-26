317df952fa74d4daa76b6bdc2d0959a76d67d814
Denis Zykov <denis.zykov.19932@yandex.ru>
2023-05-26 17:13:18 +0300

    feat: add router

--- controllers/users.js
+++ controllers/users.js
@@ -28,6 +28,21 @@ module.exports.getUser = (req, res) => {
     })
 }
 
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body
+  User.create({ name, about, avatar })
+    .then((user) => res.status(201).send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({
+          message: 'Переданы некорректные данные при создании пользователя.',
+        })
+      } else {
+        res.status(500).send({ message: 'Внутренняя ошибка сервера'})
+      }
+    })
+}
+
 module.exports.updateProfile = (req, res) => {
   const { name, about } = req.body
   User.findByIdAndUpdate(
@@ -55,11 +70,7 @@ module.exports.updateProfile = (req, res) => {
 
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body
-  User.findByIdAndUpdate(
-      req.user._id,
-      { avatar },
-      { new: true }
-    )
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
     .then((user) => res.status(200).send(user))
     .catch((err) => {
       if (err.name === 'CastError' || err.name === 'ValidationError') {
---
+++ routes/cards.js
@@ -0,0 +1,16 @@
+const router = require('express').Router()
+
+const {
+  getCards,
+  createCard,
+  deleteCard,
+  toggleLike,
+} = require('../controllers/cards')
+
+router.get('/', getCards)
+router.post('/', createCard)
+router.delete('/:cardId', deleteCard)
+router.put('/:cardId/likes', toggleLike)
+router.delete('/:cardId/likes', (req, res) => toggleLike(req, res, false))
+
+module.exports = router
